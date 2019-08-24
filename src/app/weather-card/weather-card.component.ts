161397b0176582b44441d5ade5d80403382e6263
Alexey Panashchenko <alpa@ciklum.com>
2019-08-25 00:11:50 +0300

    Add all API services

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpService} from "../http.service";
 import {Weather} from "../model/weather";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-weather-card',
@@ -9,13 +10,20 @@ import {Weather} from "../model/weather";
 })
 export class WeatherCardComponent implements OnInit {
 
+  mainSearchForm: FormGroup;
+  openWeatherForm: FormGroup;
+  weatherBitForm: FormGroup;
+  apixuForm: FormGroup;
+  formBuilder: FormBuilder;
   owpWeather: Weather;
   apixuWeather: Weather;
   weatherBitWeather: Weather;
   http: HttpService;
 
-  constructor(http: HttpService) {
+  constructor(http: HttpService, formBuilder: FormBuilder) {
     this.http = http;
+    this.formBuilder = formBuilder;
+    this.createForms();
   }
 
   ngOnInit() {
@@ -33,6 +41,65 @@ export class WeatherCardComponent implements OnInit {
     });
   }
 
+  createForms() {
+    this.mainSearchForm = this.formBuilder.group({
+      city: [''],
+    });
+    this.openWeatherForm = this.formBuilder.group({
+      city: [''],
+    });
+    this.weatherBitForm = this.formBuilder.group({
+      city: [''],
+    });
+    this.apixuForm = this.formBuilder.group({
+      city: [''],
+    });
+  }
+
+  submitMainSearch() {
+    let city = this.mainSearchForm.value['city'];
+    this.getOpenWeatherMap(city);
+    this.getWeatherBit(city);
+    this.getApixu(city);
+    this.mainSearchForm.reset();
+  }
+
+  submitOwm() {
+    this.getOpenWeatherMap(this.openWeatherForm.value['city']);
+    this.openWeatherForm.reset()
+  }
+
+  submitApixu() {
+    this.getApixu(this.apixuForm.value['city']);
+    this.apixuForm.reset()
+  }
+
+  submitWb() {
+    this.getWeatherBit(this.weatherBitForm.value['city']);
+    this.weatherBitForm.reset()
+
+  }
+
+  getOpenWeatherMap(city) {
+    this.http.getOpenWeatherMap(city).subscribe(data => {
+      this.owpWeather = data;
+      console.log(this.owpWeather);
+    });
+  }
+
+  getWeatherBit(city) {
+    this.http.getWeatherBit(city).subscribe(data => {
+      this.weatherBitWeather = data;
+      console.log(this.weatherBitWeather);
+    });
+  }
+
+  getApixu(city) {
+    this.http.getApixu(city).subscribe(data => {
+      this.apixuWeather = data;
+      console.log(this.apixuWeather);
+    });
+  }
 
 
 }
